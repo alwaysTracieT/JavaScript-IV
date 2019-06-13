cc6bc5f1525e60b57f9a6e67ee405087e7d20ef5
Tracie T <traciecodes@gmail.com>
2019-06-13 14:39:57 -0500

    Initial commit. Added prev. code and started working on classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,37 @@
 // CODE here for your Lambda Classes
+//CLASS "Game Obj" Old Way
+function GameObject(options) {
+    this.createdAt = options.createdAt;
+    this.dimensions = options.dimensions;
+    this.name = options.name;
+  }
+  
+  GameObject.prototype.destroy = function () {
+    return `${this.name} was removed from the game.`;
+  };
+  
+//CLASS "Game Obj" New Way
+class GameObject {
+    construtor(gameProperties) {
+      this.createdAt = gameProperties.createdAt;
+      this.dimensions = gameProperties.dimensions;
+    }
+    destroy() {
+      return `${this.name} was removed from the game`;
+    }
+  }
+
+//CLASS "Character Stats" New Way
+
+  class CharacterStats extends GameObject {
+    constructor(characterProperties) {
+      super(characterProperties);
+      this.hp = characterProperties.hp;
+      this.name = characterProperties.name;
+    }
+  
+     takeDamage() {
+      return `${this.name} took damage`;
+    }
+  }
+
