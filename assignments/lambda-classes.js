b89fa6a40efa85f30e2a1d3b955eca347c810844
Tracie T <traciecodes@gmail.com>
2019-06-13 15:49:24 -0500

    Finished setting up lambda-classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,37 +1,65 @@
 // CODE here for your Lambda Classes
-//CLASS "Game Obj" Old Way
-function GameObject(options) {
-    this.createdAt = options.createdAt;
-    this.dimensions = options.dimensions;
-    this.name = options.name;
-  }
-  
-  GameObject.prototype.destroy = function () {
-    return `${this.name} was removed from the game.`;
-  };
-  
-//CLASS "Game Obj" New Way
-class GameObject {
-    construtor(gameProperties) {
-      this.createdAt = gameProperties.createdAt;
-      this.dimensions = gameProperties.dimensions;
-    }
-    destroy() {
-      return `${this.name} was removed from the game`;
-    }
-  }
+//Person Class (Base-class)
+class Person {
+    constructor(personAtts) {
+    this.name = personAtts.name;
+    this.age = personAtts.age;
+    this.location = personAtts.location;
+    this.gender = personAtts.gender;
+    };
 
-//CLASS "Character Stats" New Way
+    speak(){
+        return `Hello my name is ${this.name}, I am from ${this.location}.`
+    };
+};
 
-  class CharacterStats extends GameObject {
-    constructor(characterProperties) {
-      super(characterProperties);
-      this.hp = characterProperties.hp;
-      this.name = characterProperties.name;
-    }
-  
-     takeDamage() {
-      return `${this.name} took damage`;
-    }
-  }
+//Instructor Class
+class Instructor extends Person {
+    constructor(instructorAtts) {
+        super(instructorAtts);
+        this.specialty = instructorAtts.specialty;
+        this.favLanguage = instructorAtts.favLanguage;
+        this.catchPhrase = instructorAtts.catchPhrase;
+    };
 
+    demo(subject) {
+        return `Today we're going to learn about ${subject}`;
+    };
+    grade(student, subject) {
+        return `${student.name} receives a perfect score in ${subject}.`
+    };
+};
+
+//Student Class
+class Student extends Person {
+    constructor(studentAtts) {
+        super(studentAtts);
+        this.previousBackground = studentAtts.previousBackground;
+        this.className = studentAtts.className;
+        this.favSubjects = studentAtts.favSubjects;
+    };
+    listsSubjects() {
+        this.favSubjects.map(item => console.log(item));
+    };
+    PRAssignment() {
+        return `${this.name} has submitted a PR for ${subject}.`;
+    };
+    sprintChallenge() {
+        return `${this.name} has begun the sprint challenge for ${subject}.`;
+    };
+};
+
+//PM Class
+class ProjectManager extends Person {
+    constructor(pmAtts) {
+        super(pmAtts);
+        this.gradClassName = pmAtts.gradClassName;
+        this.favInstructor = pmAtts.gradClassName;
+    };
+    standUp(slackChannel){
+        return `${this.name} announces to ${slackChannel}, '@channel It's STANDY TIMES!'`
+    };
+    debugsCode(){
+        return `${this.name} debugs ${student.name}'s code on ${subject}. Earn those dollars, ${this.name}!`
+    };
+};
